692d59398a819fd769565b499be6f07a3b79a4f4
erikri84 <erik.garcia@saei.mx>
2017-12-13 12:03:24 -0800

    Add files via upload

---
+++ GouraudeShading/src/Application.cpp
@@ -0,0 +1,88 @@
+#include "Application.h"
+#include <iostream>
+#include <vector>
+
+#include "GL\glew.h"
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#include "ShaderFuncs.h"
+#include "glm\vec3.hpp"
+
+Application::Application()
+{}
+
+Application::~Application()
+{}
+
+void Application::setup()
+{
+	oPlane.createPlane(100); // AQUI SE CREA UN PLANO
+	loadShaders();
+
+	InitializeProgram(oPlane.shader, strVertexShader, strFragmentShader);
+	oPlane.uTransform[0] = glGetUniformLocation(oPlane.shader, "mTransform");
+
+	eye = glm::vec3(100.0f, 100.0f, 0.0f);
+	target = glm::vec3(0.0f, 0.0f, 0.0f);
+	up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+	glGenVertexArrays(1, &oPlane.vao);
+	glBindVertexArray(oPlane.vao);
+	glGenBuffers(1, &oPlane.vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, oPlane.vbo);
+
+	glBufferData(GL_ARRAY_BUFFER, oPlane.getVertexSizeInBytes() + oPlane.getTextureCoordsSizeInBytes(), NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, oPlane.getVertexSizeInBytes(), oPlane.plane);
+	glBufferSubData(GL_ARRAY_BUFFER, oPlane.getVertexSizeInBytes(), oPlane.getTextureCoordsSizeInBytes(), oPlane.textureCoords);
+	oPlane.cleanMemory();
+
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)oPlane.getVertexSizeInBytes());
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+
+	glEnable(GL_DEPTH_TEST);
+}
+
+void Application::update()
+{
+	oPlane.fAngleY = oPlane.fAngleY + 0.01f;
+}
+
+
+void Application::display()
+{
+	//Borramos el buffer de color
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//glUseProgram(oPlane.shader);
+	glBindVertexArray(oPlane.vao);
+
+	glm::mat4 myAllTransformations;
+	rotationX = glm::rotate(glm::mat4(1.0f), (oPlane.fAngleX * 3.141592f / 180.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	rotationY = glm::rotate(glm::mat4(1.0f), (oPlane.fAngleY * 3.141592f / 180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	rotationZ = glm::rotate(glm::mat4(1.0f), (oPlane.fAngleZ * 3.141592f / 180.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	rotation = rotationX * rotationY * rotationZ;
+	cam = glm::lookAt(eye, target, up);
+	pers = glm::perspective(45.0f, (640.0f / 480.0f), 0.1f, 180.0f);
+	rotation = pers * cam * rotation;
+
+	glUseProgram(oPlane.shader);
+	glUniformMatrix4fv(oPlane.uTransform[0], 1, GL_FALSE, glm::value_ptr(rotation));
+
+	glDrawArrays(GL_TRIANGLES, 0, oPlane.getNumVertex());
+}
+
+void Application::reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+}
+
+void Application::loadShaders()
+{
+	strVertexShader = loadTextFile("Shaders/gouradPlane.v");
+	strFragmentShader = loadTextFile("Shaders/passThru.f");
+}
